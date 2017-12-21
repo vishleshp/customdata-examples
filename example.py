b6b9b349b3e5c0c496e5450cca26e4f3173e918c
vishleshp <vishlesh.patel@nyu.edu>
2017-12-21 13:02:31 -0500

    fixing merge lines

--- example.py
+++ example.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-#!/usr/bin/env python                                                                                        import sys
+#!/usr/bin/env python
+import sys
 
 arg1 = sys.argv[1]  
 with open('test_output.txt', 'w') as f:
     f.write("hello!! {}. this is python script running on day 0".format(arg1))
-=======
-#!/usr/bin/env python
-
-with open('test_output.txt', 'w') as f:
-    f.write("hello!! this was a test")
-
->>>>>>> c77741887c727dfc3f70d4e0620e8937863eaeb5
-
