7e08bd6ed652c0231ea5ed3826aca15e56f35a75
vishleshp <22842112+vishleshp@users.noreply.github.com>
2018-01-19 09:57:38 -0500

    Update and rename example.py to example2.py

--- example2.py
+++ example2.py
@@ -2,5 +2,5 @@
 import sys
 
 arg1 = sys.argv[1]  
-with open('example1-output.txt', 'w') as f:
+with open('example2-output.txt', 'w') as f:
     f.write("hello!! {}. this is python script running on day 0".format(arg1))
